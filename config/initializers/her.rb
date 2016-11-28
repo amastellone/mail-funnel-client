76ccf9f31ea9e2b8695aba5b02df3fc64b7409a6
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-11-28 02:52:28 -0500

    her.rb

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -7,10 +7,25 @@ Her::API.setup url: "http://localhost:3001" do |c|
 	# # middleware to make a json request by default
 	# c.use Her::Middleware::AcceptJSON
 
+	# # Request
+	# c.use FaradayMiddleware::EncodeJson
+	# # Response
+	# c.use Her::Middleware::JsonApiParser
+	# # Adapter
+	# c.use Faraday::Adapter::NetHttp
+
 	# Request
-	c.use FaradayMiddleware::EncodeJson
+	# c.use Faraday::Request::UrlEncoded
+	# c.use FaradayMiddleware::EncodeJson
+	# c.use Her::Middleware::DefaultParseJSON
+	c.use Her::Middleware::AcceptJSON
+
 	# Response
-	c.use Her::Middleware::JsonApiParser
+	c.use Her::Middleware::DefaultParseJSON
+	# c.use Her::Middleware::JsonApiParser
+
 	# Adapter
-	c.use Faraday::Adapter::NetHttp
+	# c.use Faraday::Adapter::NetHttp
+	c.use Her::Middleware::AcceptJSON
+
 end
