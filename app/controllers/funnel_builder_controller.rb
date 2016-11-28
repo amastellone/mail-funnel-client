2d7e55e3d213fa60f6f22c91b95eee4b965747bc
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-11-28 00:57:52 -0500

    Updated Controllersand mainly implemented Her functionality

--- app/controllers/funnel_builder_controller.rb
+++ app/controllers/funnel_builder_controller.rb
@@ -1,7 +1,41 @@
 class FunnelBuilderController < ApplicationController
 
 	def index
+		# require 'rest-client'
+		serverurl = 'http://localhost:3001/api/'
+		# RestClient.post serverurl + '', { app_id: 'one', nested: { param2: 'two' } }
+
+		@tabs = []
+		hooks = ["cart", "checkout", "order"]
+		@chartdata = ""
+
+
+		hooks.each do |hook_type|
+			chartdata = StringIO.new
+			chartdata << "graph TB\n" #\n
+
+			first = true
+
+			HooksConstant.where(hook_type: hook_type).find_each do |hook|
+				if first
+					chartdata << "subgraph " << hook.hook_type << " Hook\n" #\n
+					first = false
+				end
+
+				campaign   = Campaign.where(hook: hook.id).first
+				identifier = hook.identifier
+				# Get jobs and build hashmap key / value = local_campaign
+				# Loop through server Jobs
+				# if Jobs.hook_identifier = identifier
+				chartdata << identifier << "[" << hook.name << "]" << "==>"
+				chartdata << "end\n" #\n
+				# @tabs[] << chartdata
+
+				# first = true
+				# chartdata = StringIO.new
+			end
+			@chartdata << chartdata.string
+		end
 
 	end
-
 end
--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -4,5 +4,6 @@ module ShopifyApp
     before_action :login_again_if_different_shop
     around_action :shopify_session
     layout ShopifyApp.configuration.embedded_app? ? 'embedded_app' : 'application'
+
   end
 end
