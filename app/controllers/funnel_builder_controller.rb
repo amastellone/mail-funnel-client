082817f9352da437750ea9dca42b3a142009c657
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-02 14:48:07 -0500

    Added javascript functionality to create new jobs to the UI (needs some work), in a way that is parseable when the chart is saved and parsed and server is updated. Updated App Model for testing to just use static shop-name

--- app/controllers/funnel_builder_controller.rb
+++ app/controllers/funnel_builder_controller.rb
@@ -14,67 +14,60 @@ class FunnelBuilderController < ApplicationController
 
 		require 'htmlentities'
 
-
-		@tabs = []
-		hooks = ["cart", "checkout", "order"]
+		@hooks = HooksConstant.where(hook_type: hook_type)
 		@chartdata = ""
 		@local_jobs = Array.new
 		@local_jobs_hash = Hash.new
 
-		hooks.each do |hook_type|
+		first = true
+		@hooks.find_each do |hook|
 			chartdata = StringIO.new
 			chartdata << "graph TB" << "\n"
 
-			first = true
-
-			HooksConstant.where(hook_type: hook_type).find_each do |hook|
-				if first
-					chartdata << "subgraph " << hook.hook_type << " Hook" << "\n"
-					first = false
-				end
-
-				campaign   = Campaign.where(hook: hook.id).first
-				identifier = hook.identifier
-
-				chartdata << identifier << "[" << HTMLEntities.new.encode(hook.name) << "]" << "\n"
-
-				first = true
-				count = 0
-				Job.where(app_id: appid, hook_identifier: identifier).find_each do |j|
-					jl = JobLocal.new
-					jl.local_identifier = "job_" + identifier + "_" + count
-					jl.hook_identifier = identifier
-					jl.subject = j.subject
-					jl.content = j.content
-					jl.email_list_id = j.email_list_id
-					jl.name = j.name
-
-					chartdata << "job_" << identifier << "_" << count
-					chartdata << "[" << HTMLEntities.new.encode(job.name) << "]" << "\n"
-
-					if first
-						chartdata << identifier << "-->" << jk.local_identifier << "\n"
-						first = false
-					else
-						chartdata << jl.local_identifier << "-->"
-						chartdata << "job_" << identifier << "_" << (count - 1) << "\n"
-					end
-					
-					chartdata << "click " << jl.local_identifier << " callback_" << jl.local_identifier << "\n"
-
-					@local_jobs.push(jl)
-					count = count + 1
-				end
-
-				chartdata << "end" << "\n"
-				# @tabs[] << chartdata
-
-				# first = true
-				# chartdata = StringIO.new
+			if first
+				chartdata << "subgraph " << hook.hook_type << " Hook" << "\n"
+				first = false
 			end
+
+			campaign   = Campaign.where(hook: hook.id).first
+			identifier = hook.identifier
+
+			chartdata << identifier << "[" << HTMLEntities.new.encode(hook.name) << "]" << "\n"
+
+			firstjob = true
+			count = 0
+			Job.where(app_id: appid, hook_identifier: identifier).find_each do |j|
+				jl = JobLocal.new
+				jl.local_identifier = "job_" + identifier + "_" + count
+				jl.hook_identifier = identifier
+				jl.subject = j.subject
+				jl.content = j.content
+				jl.email_list_id = j.email_list_id
+				jl.name = j.name
+
+				chartdata << "job_" << identifier << "_" << count
+				chartdata << "[" << HTMLEntities.new.encode(job.name) << "]" << "\n"
+
+				if firstjob
+					chartdata << identifier << "-->" << jk.local_identifier << "\n"
+					firstjob = false
+				else
+					chartdata << jl.local_identifier << "-->"
+					chartdata << "job_" << identifier << "_" << (count - 1) << "\n"
+				end
+
+				chartdata << "click " << jl.local_identifier << " callback_" << jl.local_identifier << "\n"
+
+				@local_jobs.push(jl)
+				count = count + 1
+			end
+
+			chartdata << "%% HOOK_APPEND_" << jl.hook_identifier << "\n"
+
+			chartdata << "end" << "\n"
+			# chartdata = StringIO.new
+		end
 			@chartdata << chartdata.string
 			# @chardata << HTMLEntities.new.encode(chardata.string)
-		end
-
 	end
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -16,8 +16,10 @@ class App
 	# has_many :emails
 
 	def myId()
+		# TODO: Verify this works / get it working
 		# Get Shopify App Name
-		name = ShopifyAPI::Store::name
+		# name = ShopifyAPI::Store::name
+		name = 'bluehelmet-dev'
 		return App.where(name: name).first.id
 	end
 
