322a92cc887438ada63cb97dfc558f83d3e14ce7
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-02 19:45:45 -0500

    Seeds commented out that do not work yet (ORM)

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,40 +33,40 @@ puts "Campaign + Hook Created: " + campaign.name.to_s
 order_update_hook = HooksConstant.create(hook_type: 'order', name: 'Update', identifier: 'order_update');
 campaign = Campaign.create(name: 'Order / Update', hooks_constant_id: order_update_hook.id, hook_identifier: order_update_hook.identifier);
 puts "Campaign + Hook Created: " + campaign.name.to_s
-
-# Create
-app = App.where(name: "bluehelmet-dev").first.id
-
-# until $x > 5 do
-	list = EmailsList.create(name:        "Main List " + $x.to_s,
-	                         description: "This is a great email list",
-	                         app_id:      app);
-
-	puts "Created List " + list.name.to_s
-	# until $x > Random.rand(3...15) do
-		email = Email.create(email:         Faker::Internet.email,
-		                     name:          Faker::Internet.name,
-		                     app_id:        app,
-		                     email_list_id: list.id);
-		puts list.name.to_s + ": Email Created " + email.email.to_s
-	# end
-	# $x +=1
+#
+# # Create
+# app = App.where(name: "bluehelmet-dev").first.id
+#
+# # until $x > 5 do
+# 	list = EmailsList.create(name:        "Main List " + $x.to_s,
+# 	                         description: "This is a great email list",
+# 	                         app_id:      app);
+#
+# 	puts "Created List " + list.name.to_s
+# 	# until $x > Random.rand(3...15) do
+# 		email = Email.create(email:         Faker::Internet.email,
+# 		                     name:          Faker::Internet.name,
+# 		                     app_id:        app,
+# 		                     email_list_id: list.id);
+# 		puts list.name.to_s + ": Email Created " + email.email.to_s
+# 	# end
+# 	# $x +=1
+# # end
+#
+# thislist = List.all.first
+#
+# Campaign.all.each do |c|
+# 	until $x > 2 do
+# 		job = Job.create(frequency:           "execute_once",
+# 		           execute_time:        "1330",
+# 		           subject:             "Email subject",
+# 		           content:             "Email Contents",
+# 		           app_id:              app,
+# 		           campaign_identifier: c.name,
+# 		           hook_identifier:     c.hook_identifier,
+# 		           executed:            false,
+# 		           email_list_id:       List.offset(rand(List.count)).first
+# 		)
+# 		puts "Job Created for " + job.hook_identifier.to_s
+# 	end
 # end
-
-thislist = List.all.first
-
-Campaign.all.each do |c|
-	until $x > 2 do
-		job = Job.create(frequency:           "execute_once",
-		           execute_time:        "1330",
-		           subject:             "Email subject",
-		           content:             "Email Contents",
-		           app_id:              app,
-		           campaign_identifier: c.name,
-		           hook_identifier:     c.hook_identifier,
-		           executed:            false,
-		           email_list_id:       List.offset(rand(List.count)).first
-		)
-		puts "Job Created for " + job.hook_identifier.to_s
-	end
-end
