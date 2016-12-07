06ed13ed34357092b7c1bebc148a0f63b2fbeaaa
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-07 01:00:27 -0500

    Ignore last commit's comment about the shopify controller - added custom logic to Shopify install process in their SDK controller

--- app/controllers/shopify_app/sessions_controller.rb
+++ app/controllers/shopify_app/sessions_controller.rb
@@ -1,5 +1,10 @@
 module ShopifyApp
   class SessionsController < ApplicationController
     include ShopifyApp::SessionsConcern
+
+    app = App.create(name: ShopifyApp::Shop::Name)
+
+    app_config = MailFunnelConfig.create(name: "app_name", value: ShopifyApp::Shop::Name)
+
   end
 end
