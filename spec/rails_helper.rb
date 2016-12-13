74e8ff39eda2423b54faead08fc1b2fde0a0934c
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-12-13 04:20:22 -0500

    Tests for Active Resource REST Models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,24 +36,6 @@ RSpec.configure do |config|
 	# config/initializers/spyke.rb
 	require 'multi_json'
 
-	class JSONParser < Faraday::Response::Middleware
-		def parse(body)
-			p body
-			json = MultiJson.load(body, symbolize_keys: true)
-			{
-				 data:     json[:data],
-				 metadata: json[:extra],
-				 errors:   json[:errors]
-			}
-		end
-	end
-
-# Replace Server URL wit .env Variable
-	Spyke::Base.connection = Faraday.new(url: 'http://localhost:3001') do |c|
-		c.request :json
-		c.use JSONParser
-		c.adapter Faraday.default_adapter
-	end
 
 	Dir[Rails.root.join('apps/models/*.rb')].each { |f| require f }
 
