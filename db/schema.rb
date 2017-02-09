83fff1a16e09eab14e5691195b56754595267626
Matt Twardowski <mttwardowski@gmail.com>
2017-02-08 22:29:22 -0500

    shchema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104013036) do
+ActiveRecord::Schema.define(version: 20170209025548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "job_queues", force: :cascade do |t|
+    t.datetime "created_at", :null=>false
+    t.datetime "updated_at", :null=>false
+  end
 
   create_table "mail_funnel_configs", force: :cascade do |t|
     t.string   "name",       :index=>{:name=>"index_mail_funnel_configs_on_name", :unique=>true, :using=>:btree}
